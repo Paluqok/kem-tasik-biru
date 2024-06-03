60c685e613628aa373145f1626c24208c20d084c
Paluqok <2022862506@student.uitm.edu.my>
2024-06-03 13:44:03 +0800

    uppppop

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 const { Pool } = require('pg');
+
 const pool = new Pool({
-  connectionString: "postgres://ua60mfps4cfmib:pf3b00b75fdf5338b311909dafbb5e86b1bf77fad7e2c5f7a5c231312887e38ce@c9uss87s9bdb8n.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/daobh2hbimne40",
+  max: 10, // connection limit
+  host: 'localhost',
+  user: 'postgres',
+  password: 'ktb1234',
+  database: 'booking_system',
+  port: 5432, // default PostgreSQL port
   ssl: {
     rejectUnauthorized: false
   }
