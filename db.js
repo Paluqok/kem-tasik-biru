28f7fd4e8997d044b0f171e77ef0a6ccafa09e0f
Paluqok <2022862506@student.uitm.edu.my>
2024-06-09 16:41:39 +0800

    heroku init

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   max: 10, // connection limit
   host: 'c9uss87s9bdb8n.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
   user: 'ua60mfps4cfmib',
-  password: 'ktb1234',
+  password: 'pf3b00b75fdf5338b311909dafbb5e86b1bf77fad7e2c5f7a5c231312887e38ce',
   database: 'daobh2hbimne40',
   port: 5432, // default PostgreSQL port
   ssl: {
@@ -14,7 +14,7 @@ const pool = new Pool({
 
 pool.connect((err, client, release) => {
   if (err) {
-    console.error('Error connecting to the database:', err);
+    console.error('Error connecting to the database:', err.stack);
   } else {
     console.log('Connected to the database');
     release(); // Release the client back to the pool
