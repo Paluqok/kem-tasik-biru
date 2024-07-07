7d9ac93d50a699c310bc89d541455b5119e3b19c
Paluqok <2022862506@student.uitm.edu.my>
2024-07-07 17:34:06 +0800

    s

--- src/main/java/com/heroku/java/controller/StaffController.java
+++ src/main/java/com/heroku/java/controller/StaffController.java
@@ -34,29 +34,55 @@ public class StaffController {
     }
 
     @PostMapping("/createAccountStaff")
-    public String CreateAccountStaff(@ModelAttribute("createAccountStaff") Staff staff) {
+    public String createAccountStaff(@ModelAttribute("createAccountStaff") @RequestParam("staffName") String staffName,
+                                   @RequestParam("staffEmail") String staffEmail,
+                                   @RequestParam("staffAddress") String staffAddress,
+                                   @RequestParam("staffPhoneNo") String staffPhoneNo,
+                                   @RequestParam("staffPassword") String staffPassword) throws IOException {
+        
+        Staff staff = new Staff();
+        staff.setStaffName(staffName);
+        staff.setStaffEmail(staffEmail);
+        staff.setStaffAddress(staffAddress);
+        staff.setStaffPhoneNo(staffPhoneNo);
+        staff.setStaffPassword(staffPassword);
 
-        try {
-            Connection conn = dataSource.getConnection();
-            String sql = "INSERT INTO public.staff (staffname,staffemail,staffphonenum,staffaddress,staffpassword,managerid) VALUES (?,?,?,?,?,?)";
-            PreparedStatement statement = conn.prepareStatement(sql);
-			
-			statement.setString(1, staff.getStaffName());
-			statement.setString(2, staff.getStaffEmail());
-			statement.setString(3, staff.getStaffPhoneNo());
-			statement.setString(4, staff.getStaffAddress());
-			statement.setString(5, staff.getStaffPassword());
-			 if (staff.getManagerId() != null) {
-	                statement.setInt(6, staff.getManagerId());
-	            } else {
-	                statement.setNull(6, java.sql.Types.INTEGER);
-	            }
-			
-			statement.executeUpdate();
-        } catch (SQLException e) {
+        try (Connection connection = dataSource.getConnection()) {
+
+            System.out.println("Received staff details:");
+            System.out.println("Name: " + staff.getStaffName());
+            System.out.println("Location: " + staff.getStaffEmail());
+            System.out.println("Duration: " + staff.getStaffAddress());
+            System.out.println("Price: " + staff.getStaffPhoneNo());
+            System.out.println("Image: " + staff.getStaffPassword());
+
+            String staffSql = "INSERT INTO public.staff(staffname, stafflocation, staffduration, staffprice, staffimage) VALUES (?, ?, ?, ?, ?) RETURNING staffid";
+
+            try (PreparedStatement statement = connection.prepareStatement(staffSql)) {
+                statement.setString(1, staff.getStaffName());
+                statement.setString(2, staff.getStaffEmail());
+                statement.setString(3, staff.getStaffAddress());
+                statement.setString(4, staff.getStaffPhoneNo());
+                statement.setString(5, staff.getStaffPassword()); // Set the Base64-encoded image
+
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    if (resultSet.next()) {
+                        Long staffId = resultSet.getLong("staffid");
+                        staff.setStaffId(staffId);
+                        System.out.println("Inserted staff with ID: " + staffId);
+                    } else {
+                        throw new SQLException("Failed to insert staff, no ID obtained.");
+                    }
+                }
+            }
+        }catch (SQLException e) {
             e.printStackTrace();
+            // Handle exception appropriately, e.g., log error or rethrow as runtime exception
+            throw new RuntimeException("Failed to insert staff", e);
         }
-        return "redirect:/index";
-    }
 
+        return "redirect:/staffstaff"; // Return the created staff object
+    }
+    
 }
+
