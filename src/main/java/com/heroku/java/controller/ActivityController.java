495020c134f7015c3d519150d2f22d1cb7d5d5c1
Paluqok <2022862506@student.uitm.edu.my>
2024-07-11 05:34:50 +0800

    f dbdbf

--- src/main/java/com/heroku/java/controller/ActivityController.java
+++ src/main/java/com/heroku/java/controller/ActivityController.java
@@ -284,6 +284,7 @@ public String deleteActivity(@PathVariable("id") Long activityId, HttpSession se
 
     String sql = "DELETE FROM public.activity WHERE activityid = ?";
     jdbcTemplate.update(sql, activityId);
+    if("wet".equals(activityType))
     return "redirect:/listActivity";
 }
 
